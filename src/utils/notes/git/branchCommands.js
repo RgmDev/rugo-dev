bde55a63528cc1306450754bec25cad23935c262
rgonzalez <rgonzalez@mkdautomotive.com>
2021-11-22 12:04:42 +0100

    add git branch commands markdown

---
+++ src/utils/notes/git/branchCommands.js
@@ -0,0 +1,19 @@
+export const GIT_BRANCH_COMMANDS_MD = `
+# BRANCH COMMANDS
+
+## Branches list
+    git branch 
+
+## Create new branch
+    git checkout branch-name
+
+## Delete local branch
+    git branch -d branch-name
+    git branch -D branch-name
+
+## Delete remote branch
+    git push origin --delete branch-name
+
+## Download new branches from remote
+    git fetch
+`;
--- src/utils/notes/index.js
+++ src/utils/notes/index.js
@@ -1,10 +1,18 @@
 import { HEROKU_GET_STARTED_MD } from "./heroku/getStarted";
 import { REACT_GET_STARTED_MD } from "./react/getStarted";
 import { MARKDOWN_TEMPLATE_MD } from "./markdown/template";
+import { GIT_BRANCH_COMMANDS_MD } from "./git/branchCommands";
 
-import { SiHeroku, SiReact } from 'react-icons/si';
+import { SiHeroku, SiReact, SiAmazonaws, SiGit } from 'react-icons/si';
 import { GoMarkdown } from 'react-icons/go';
 
+let aws = [
+    {content: MARKDOWN_TEMPLATE_MD, title: 'Delete from aws'}
+];
+
+let git = [
+    {content: GIT_BRANCH_COMMANDS_MD, title: 'Branch commands'}
+];
 
 let heroku = [
     {content: HEROKU_GET_STARTED_MD, title: 'Get started'},
@@ -19,6 +27,8 @@ let markdown = [
 ];
 
 export let notes = [ 
+    {notes: aws, text: 'Amazon Web Services', icon: <SiAmazonaws />, value: 'aws'},
+    {notes: git, text: 'Git', icon: <SiGit />, value: 'git'},
     {notes: heroku, text: 'Heroku', icon: <SiHeroku />, value: 'heroku'},
     {notes: react, text: 'React', icon: <SiReact />, value: 'react'},
     {notes: markdown, text: 'Markdown', icon: <GoMarkdown />, value: 'react'},
